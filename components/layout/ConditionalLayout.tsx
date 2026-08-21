fece0fdc3b5da6938307372158ba7aa0431fd827
LE TUNG <tungle07102005@gmail.com>
2026-08-21 20:26:29 +0700

    chore(layout): adjust conditional rendering rules

--- components/layout/ConditionalLayout.tsx
+++ components/layout/ConditionalLayout.tsx
@@ -1,19 +1,21 @@
-"use client"
+"use client";
 
 import { usePathname } from "next/navigation";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export function ConditionalLayout({ children }: { children: React.ReactNode }) {
-    const pathName = usePathname()
-    const hideLayout = pathName.startsWith("/auth")
-        || pathName.startsWith("/dashboard")
-        || pathName.startsWith("/editor")
-    return (
-        <>
-            {!hideLayout && <Header />}
-            {children}
-            {!hideLayout && <Footer />}
-        </>
-    )
-} 
+  const pathName = usePathname();
+  const hideLayout =
+    pathName.startsWith("/auth") ||
+    pathName.startsWith("/dashboard") ||
+    pathName.startsWith("/editor") ||
+    pathName.startsWith("/admin");   
+  return (
+    <>
+      {!hideLayout && <Header />}
+      {children}
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
